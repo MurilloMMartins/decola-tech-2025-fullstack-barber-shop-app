ce966bce3c05257e05b1bb22f74c6d43d14e1bcd
MurilloMMartins <murillomartins19@gmail.com>
2025-06-13 23:46:11 -0300

    Added client http service

--- barber-shop-ui/src/app/app.config.ts
+++ barber-shop-ui/src/app/app.config.ts
@@ -1,9 +1,17 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideNgxMask } from 'ngx-mask';
 import { routes } from './app.routes';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async'
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideHttpClient(withInterceptorsFromDi()),
+    provideNgxMask({})
+  ],
 };
--- barber-shop-ui/src/app/services/api-client/clients/clients.service.ts
+++ barber-shop-ui/src/app/services/api-client/clients/clients.service.ts
@@ -1,9 +1,42 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../../../environments/environment';
+import {
+  DetailClientResponse,
+  ListClientResponse,
+  SaveClientRequest,
+  SaveClientResponse,
+  UpdateClientRequest,
+  UpdateClientResponse,
+} from './client.models';
+import { IClientService } from './iclients.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class ClientsService {
+export class ClientsService implements IClientService {
+  private readonly basePath = environment.apiURL;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  save(request: SaveClientRequest): Observable<SaveClientResponse> {
+    return this.http.post<SaveClientResponse>(`${this.basePath}clients`, request);
+  }
+
+  update(id: number, request: UpdateClientRequest): Observable<UpdateClientResponse> {
+    return this.http.put<UpdateClientResponse>(`${this.basePath}clients/${id}`, request);
+  }
+
+  delete(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.basePath}clients/${id}`);
+  }
+
+  list(): Observable<ListClientResponse[]> {
+    return this.http.get<ListClientResponse[]>(`${this.basePath}clients`);
+  }
+
+  findByID(id: number): Observable<DetailClientResponse> {
+    return this.http.get<DetailClientResponse>(`${this.basePath}clients/${id}`);
+  }
 }
