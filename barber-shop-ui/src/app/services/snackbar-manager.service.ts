99c6edab233b8686f4bb488342b8a952c6ca07a8
MurilloMMartins <murillomartins19@gmail.com>
2025-06-14 22:08:41 -0300

    Added client registering

--- barber-shop-ui/src/app/services/snackbar-manager.service.ts
+++ barber-shop-ui/src/app/services/snackbar-manager.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { ISnackBarManagerService } from './isnackbar-manager.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class SnackbarManagerService {
+export class SnackbarManagerService implements ISnackBarManagerService {
+  constructor(private readonly snackBar: MatSnackBar) {}
 
-  constructor() { }
+  show(
+    message: string,
+    action: string = 'fechar',
+    duration: number = 3000
+  ): void {
+    this.snackBar.open(message, action, {duration, verticalPosition: 'top', horizontalPosition: 'right'})
+  }
 }
